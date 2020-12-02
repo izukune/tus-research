e9177c67cb04ab925b2cdb7d70e7ab6cd0accd63
izukune <izuizuizu1009@gmail.com>
2020-12-03 00:09:44 +0900

    main source code

--- Main/perceptron.py
+++ Main/perceptron.py
@@ -1,30 +1,22 @@
-import numpy as np
-
 def AND(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0,5])
     b = -0.7
-    tmp = np.sum(w*x) + b
+    tmp = x1 * 0.5 + x2 * 0.5 + b
     if tmp <= 0:
         return 0
     else:
         return 1
 
 def NAND(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([-0.5, -0,5])
     b = 0.7
-    tmp = np.sum(w*x) + b
+    tmp = x1 * (-0.5) + x2 * (-0.5) + b
     if tmp <= 0:
         return 0
     else:
         return 1
 
 def OR(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0,5])
     b = -0.3
-    tmp = np.sum(w*x) + b
+    tmp = x1 * 0.5 + x2 * 0.5 + b
     if tmp <= 0:
         return 0
     else:
@@ -35,3 +27,10 @@ def XOR(x1, x2):
     s2 = OR(x1, x2)
     y = AND(s1, s2)
     return y
+
+if __name__ == "__main__":
+    print(XOR(0, 0))
+    print(XOR(1, 0))
+    print(XOR(0, 1))
+    print(XOR(1, 1))
+    pass

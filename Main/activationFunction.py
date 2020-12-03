734c8881767f4fd9b5755f72be3f08b1df7d5859
izukune <izuizuizu1009@gmail.com>
2020-12-03 14:45:25 +0900

    commit activationFunction

---
+++ Main/activationFunction.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#step function
+def step_function(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
+
+#sigmoid function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+#ReLU function
+def relu(x):
+    return np.maximum(0, x)
+
